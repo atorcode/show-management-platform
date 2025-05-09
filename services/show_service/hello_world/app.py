0baf3f618e0c2f76c27adac9bc9938d6dd954f5c
atorcode <atorcode@gmail.com>
2025-05-09 10:47:52 -0700

    Fix for real this time

--- services/show_service/hello_world/app.py
+++ services/show_service/hello_world/app.py
@@ -43,17 +43,17 @@ def lambda_handler(event: dict, context: dict) -> dict:
 
     if http_method == "GET":
         if path.startswith("/shows/"):
-            return {"statusCode": 200, "message": "GET BY ID"}
+            return {"statusCode": 200, "body": {"message": "GET BY ID"}}
         if path == "/shows":
-            return {"statusCode": 200, "message": "GET MANY"}
+            return {"statusCode": 200, "body": {"message": "GET MANY"}}
 
     if http_method == "POST" and path == "/shows":
-        return {"statusCode": 200, "message": "POST SHOW"}
+        return {"statusCode": 200, "body": {"message": "POST SHOW"}}
 
     if http_method == "PUT" and path.startswith("/shows/"):
-        return {"statusCode": 200, "message": "PUT/UPDATE SHOW"}
+        return {"statusCode": 200, "body": {"message": "PUT/UPDATE SHOW"}}
 
     if http_method == "DELETE" and path.startswith("/shows/"):
         return {"statusCode": 204}
 
-    return {"statusCode": 404, "message": "CATCH-ALL ERROR ROUTE"}
+    return {"statusCode": 404, "body": {"message": "CATCH-ALL ERROR ROUTE"}}
