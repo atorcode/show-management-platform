654b72155dcb99f1dbe36c1e8ffd67d30a21f6de
atorcode <atorcode@gmail.com>
2025-05-09 11:39:36 -0700

    Fix proxy paths

--- services/show_service/hello_world/app.py
+++ services/show_service/hello_world/app.py
@@ -40,21 +40,28 @@ import json
 
 def lambda_handler(event: dict, context: dict) -> dict:
     http_method = event["httpMethod"]
-    path = event["path"]
+
+    proxy = ""  # the path after /shows
+    if (
+        "pathParameters" in event
+        and event["pathParameters"]
+        and "proxy" in event["pathParameters"]
+    ):
+        proxy = event["pathParameters"]["proxy"]
 
     if http_method == "GET":
-        if path.startswith("/shows/"):
+        if proxy and proxy != "":  # if there's something after /shows/
             return {"statusCode": 200, "body": json.dumps({"message": "GET BY ID"})}
-        if path == "/shows":
+        if not proxy:
             return {"statusCode": 200, "body": json.dumps({"message": "GET MANY"})}
 
-    if http_method == "POST" and path == "/shows":
+    if http_method == "POST" and not proxy:
         return {"statusCode": 200, "body": json.dumps({"message": "POST SHOW"})}
 
-    if http_method == "PUT" and path.startswith("/shows/"):
+    if http_method == "PUT" and proxy:
         return {"statusCode": 200, "body": json.dumps({"message": "PUT/UPDATE SHOW"})}
 
-    if http_method == "DELETE" and path.startswith("/shows/"):
+    if http_method == "DELETE" and proxy:
         return {"statusCode": 204}
 
     return {"statusCode": 404, "body": json.dumps({"message": "CATCH-ALL ERROR ROUTE"})}
